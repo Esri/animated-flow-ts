0c1c03d225c083384afc4d5342bbb9c06bd3caa6
Dario D'Amico <ddamico@esri.com>
2021-07-29 21:35:22 -0700

    wip basic architecture

--- src/wind/LayerView2D.ts
+++ src/wind/LayerView2D.ts
@@ -5,7 +5,63 @@ import BaseLayerView2D from "../base/LayerView2D";
 import WindVisualization from "./Visualization";
 
 @subclass("wind-es.wind.LayerView2D")
-export default class LayerView2D extends BaseLayerView2D {
+export default class LayerView2D extends BaseLayerView2D {  
+  private _programs: HashMap<{ program: WebGLProgram; uniforms: HashMap<WebGLUniformLocation>; }> | null = null;
+
+  initialize(): void {
+    // TODO!
+    // Async?
+    const vertexSource = `
+      attribute vec4 a_Position;
+
+      uniform mat4 u_ScreenFromLocal;
+      uniform mat4 u_ClipFromScreen;
+
+      varying vec2 v_TexCoord;
+      
+      void main(void) {
+        gl_Position = u_ClipFromScreen * u_ScreenFromLocal * a_Position;
+        v_TexCoord = a_Position.xy;
+      }`;
+
+    const fragmentSource = `
+      precision mediump float;
+
+      varying vec2 v_TexCoord;
+
+      void main(void) {
+        gl_FragColor = vec4(v_TexCoord, 0.0, 1.0);
+      }`;
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    defined(vertexShader);
+    gl.shaderSource(vertexShader, vertexSource);
+    gl.compileShader(vertexShader);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    defined(fragmentShader);
+    gl.shaderSource(fragmentShader, fragmentSource);
+    gl.compileShader(fragmentShader);
+
+    const program = gl.createProgram();
+    defined(program);
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.bindAttribLocation(program, 0, "a_Position");
+    gl.linkProgram(program);
+    gl.deleteShader(vertexShader);
+    gl.deleteShader(fragmentShader);
+
+    this._programs = {
+      solid: {
+        program,
+        uniforms: {
+          u_ScreenFromLocal: gl.getUniformLocation(program, "u_ScreenFromLocal")!,
+          u_ClipFromScreen: gl.getUniformLocation(program, "u_ClipFromScreen")!
+        }
+      }
+    };
+  }
+  
   loadVisualization(extent: Extent, resolution: number): Promise<Visualization> {
     return new Promise<Visualization>((resolve) => {
       setTimeout(() => {
--- src/wind/Visualization.ts
+++ src/wind/Visualization.ts
@@ -5,7 +5,6 @@ import BaseVisualization from "../base/Visualization";
 import { defined } from "../util";
 
 export default class Visualization extends BaseVisualization {
-  private _programs: HashMap<{ program: WebGLProgram; uniforms: HashMap<WebGLUniformLocation>; }> | null = null;
   private _vertexBuffer: WebGLBuffer | null = null;
   private u_ScreenFromLocal = mat4.create();
   private u_ClipFromScreen = mat4.create();
@@ -15,56 +14,6 @@ export default class Visualization extends BaseVisualization {
   }
 
   attach(gl: WebGLRenderingContext): void {
-    const vertexSource = `
-      attribute vec4 a_Position;
-
-      uniform mat4 u_ScreenFromLocal;
-      uniform mat4 u_ClipFromScreen;
-
-      varying vec2 v_TexCoord;
-      
-      void main(void) {
-        gl_Position = u_ClipFromScreen * u_ScreenFromLocal * a_Position;
-        v_TexCoord = a_Position.xy;
-      }`;
-
-    const fragmentSource = `
-      precision mediump float;
-
-      varying vec2 v_TexCoord;
-
-      void main(void) {
-        gl_FragColor = vec4(v_TexCoord, 0.0, 1.0);
-      }`;
-
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    defined(vertexShader);
-    gl.shaderSource(vertexShader, vertexSource);
-    gl.compileShader(vertexShader);
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    defined(fragmentShader);
-    gl.shaderSource(fragmentShader, fragmentSource);
-    gl.compileShader(fragmentShader);
-
-    const program = gl.createProgram();
-    defined(program);
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.bindAttribLocation(program, 0, "a_Position");
-    gl.linkProgram(program);
-    gl.deleteShader(vertexShader);
-    gl.deleteShader(fragmentShader);
-
-    this._programs = {
-      solid: {
-        program,
-        uniforms: {
-          u_ScreenFromLocal: gl.getUniformLocation(program, "u_ScreenFromLocal")!,
-          u_ClipFromScreen: gl.getUniformLocation(program, "u_ClipFromScreen")!
-        }
-      }
-    };
-
     const vertexBuffer = gl.createBuffer();
     defined(vertexBuffer);
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
