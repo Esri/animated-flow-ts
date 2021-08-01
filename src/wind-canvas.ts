0709517ce0da4435dc9f69b0fa563874ba7df76f
Dario D'Amico <ddamico@esri.com>
2021-08-01 09:17:13 -0700

    webgl lines

--- src/wind-canvas.ts
+++ src/wind-canvas.ts
@@ -10,6 +10,7 @@ function smooth(data: Float32Array, width: number, height: number, sigma: number
   const horizontal = new Float32Array(data.length);
 
   const halfRadius = Math.round(3 * sigma);
+  console.log("halfRadius", halfRadius);
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
@@ -54,12 +55,67 @@ function smooth(data: Float32Array, width: number, height: number, sigma: number
   return final;
 }
 
-function getFlowLines(pixelBlock: any): { position: [number, number]; distance: number; time: number; }[][] {
+function symbolize(pixelBlock: any): ImageData {
+  // const W = pixelBlock.width;
+  // const H = pixelBlock.height;
+  // const POWER = 0.1;
+  // const DECAY = 0.0025;
+  // const HALF_RADIUS = 100;
+  // const data = new Float32Array(W * H * 3);
+  
+  // for (let i = 0; i < 1000; i++) {
+  //   const x = Math.round(Math.random() * W);
+  //   const y = Math.round(Math.random() * H);
+  //   const vx = POWER * (Math.random() * 2 - 1);
+  //   const vy = POWER * (Math.random() * 2 - 1);
+    
+  //   for (let xi = x - HALF_RADIUS; xi <= x + HALF_RADIUS; xi++) {
+  //     if (xi < 0 || xi >= W) {
+  //       continue;
+  //     }
+
+  //     for (let yi = y - HALF_RADIUS; yi <= y + HALF_RADIUS; yi++) {
+  //       if (yi < 0 || yi >= H) {
+  //         continue;
+  //       }
+
+  //       const weight = Math.exp(-DECAY * ((x - xi) * (x - xi) + (y - yi) * (y - yi)));
+        
+  //       data[3 * (yi * W + xi) + 0] += vx * weight;
+  //       data[3 * (yi * W + xi) + 1] += vy * weight;
+  //       data[3 * (yi * W + xi) + 2] += weight;
+  //     }
+  //   }
+  // }
+
+  // for (let i = 0; i < W * H; i++) {
+  //   if (data[3 * i + 2]! > 0.01) {
+  //     data[3 * i + 0] /= data[3 * i + 2]!;
+  //     data[3 * i + 1] /= data[3 * i + 2]!;
+  //     data[3 * i + 2] = 1;
+  //   } else {
+  //     data[3 * i + 0] = 0;
+  //     data[3 * i + 1] = 0;
+  //     data[3 * i + 2] = 0;
+  //   }
+  // }
+
   const W = pixelBlock.width;
   const H = pixelBlock.height;
   const rawData = new Float32Array(W * H * 2);
 
+  // let minMag = 1000000;
+  // let maxMag = -1000000;
+  // let minDir = 1000000;
+  // let maxDir = -1000000;
+  
   for (let i = 0; i < W * H; i++) {
+
+    //   this.imageData.data[4 * i + 0] = pixelBlock.pixels[0][i];
+    //   this.imageData.data[4 * i + 1] = pixelBlock.pixels[1][i];
+    //   this.imageData.data[4 * i + 2] = 0;
+    //   this.imageData.data[4 * i + 3] = 255;
+
     const mag = pixelBlock.pixels[0][i] / 10;
     const dir = Math.PI * pixelBlock.pixels[1][i] / 180;
 
@@ -67,18 +123,57 @@ function getFlowLines(pixelBlock: any): { position: [number, number]; distance: 
     const si = Math.sin(dir);
     const u = co * mag + si * mag;
     const v = -si * mag + co * mag;
-    
+
+    // minMag = Math.min(minMag, mag);
+    // minDir = Math.min(minDir, dir);
+    // maxMag = Math.max(maxMag, mag);
+    // maxDir = Math.max(maxDir, dir);
+
     rawData[2 * i + 0] = u;
     rawData[2 * i + 1] = v;
+    
+
+    // data[3 * i + 0] = Math.random() * 2 - 1;
+    // data[3 * i + 1] = Math.random() * 2 - 1;
+    // data[3 * i + 2] = 1;
   }
 
   const data = smooth(rawData, W, H, 10);
 
-  /////////////////////////////////////////////////////
+  // console.log(minMag, maxMag, minDir, maxDir);
+
+  // const COLOR_SCALE = 1;
+
+  // function clamp(x: number): number {
+  //   if (x < -1) {
+  //     return -1;
+  //   }
+  //   if (x > 1) {
+  //     return 1;
+  //   }
+  //   return x;
+  // }
+
+  const imageData = new ImageData(W, H);
+  // for (let i = 0; i < W * H; i++) {
+  //   const vx = data[2 * i + 0]!;
+  //   const vy = data[2 * i + 1]!;
+  //   imageData.data[4 * i + 0] = Math.round(127.5 + 127.5 * clamp(vx) * COLOR_SCALE);
+  //   imageData.data[4 * i + 1] = Math.round(127.5 + 127.5 * clamp(vy) * COLOR_SCALE);
+  //   imageData.data[4 * i + 2] = 255;
+  //   imageData.data[4 * i + 3] = 100;
+  // }
+
+  const canvas = document.createElement("canvas");
+  canvas.width = W;
+  canvas.height = H;
+  canvas.style.border = "1px solid black";
+  const ctx = canvas.getContext("2d")!;
+  ctx.putImageData(imageData, 0, 0);
 
   function f(x: number, y: number): [number, number] {
-    const X = Math.round(x);
-    const Y = Math.round(y);
+    const X = Math.floor(x);
+    const Y = Math.floor(y);
     if (X < 0 || X >= W) {
       return [0, 0];
     }
@@ -87,29 +182,22 @@ function getFlowLines(pixelBlock: any): { position: [number, number]; distance: 
     }
     return [data[2 * (Y * W + X) + 0]!, data[2 * (Y * W + X) + 1]!];
   }
-  
-  /////////////////////////////////////////////////////
 
-  const lines: { position: [number, number]; distance: number; time: number; }[][] = [];
+  ctx.fillStyle = "rgba(0, 0, 0, 0.6)";
+  ctx.strokeStyle = "rgba(0, 0, 0, 0.6)";
 
-  // const SEGMENT_LENGTH = 0.1;
-  const SEGMENT_LENGTH = 10;
+  const SEGMENT_LENGTH = 0.1;
+
+  ctx.strokeStyle = "rgba(60, 160, 220, 0.2)";
 
   function trace(field: (x: number, y: number) => [number, number], x0: number, y0: number) {
-    const line: { position: [number, number]; distance: number; time: number; }[] = [];
-
+    ctx.beginPath();
+    ctx.moveTo(x0, y0);
     let t = 0;
-    let d = 0;
     let c = 0;
     
     let x = x0;
     let y = y0;
-
-    line.push({
-      position: [x, y],
-      distance: d,
-      time: t
-    });
     
     while (t < 1000 && c < 1000) {
       const [vx, vy] = field(x, y);
@@ -123,28 +211,18 @@ function getFlowLines(pixelBlock: any): { position: [number, number]; distance: 
       y += dy * SEGMENT_LENGTH;
       const dt = SEGMENT_LENGTH / v;
       t += dt;
-      d += SEGMENT_LENGTH;
+      ctx.lineTo(x, y);
       c++;
-
-      line.push({
-        position: [x, y],
-        distance: d,
-        time: t
-      });
     }
-
-    lines.push(line);
+    
+    ctx.stroke();
   }
 
-  // for (let i = 0; i < 10; i++) {
-  //   trace(f, Math.round(Math.random() * W), Math.round(Math.random() * H));
-  // }
-
-  for (let i = 0; i < 5000; i++) {
+  for (let i = 0; i < 20000; i++) {
     trace(f, Math.round(Math.random() * W), Math.round(Math.random() * H));
   }
-  
-  return lines;
+
+  return ctx.getImageData(0, 0, W, H);
 }
 
 export class SharedResources extends BaseSharedResources {
@@ -156,33 +234,33 @@ export class SharedResources extends BaseSharedResources {
   override attach(gl: WebGLRenderingContext): void {
     const vertexSource = `
       attribute vec2 a_Position;
-      attribute vec2 a_Extrude;
-      attribute float a_Side;
+      attribute vec2 a_TexCoord;
       
       uniform mat4 u_ScreenFromLocal;
-      uniform mat4 u_Rotation;
       uniform mat4 u_ClipFromScreen;
-
-      varying float v_Side;
+      
+      varying vec2 v_TexCoord;
       
       void main(void) {
-        vec4 screenPosition = u_ScreenFromLocal * vec4(a_Position, 0.0, 1.0);
-        screenPosition += u_Rotation * vec4(a_Extrude, 0.0, 0.0);
-        gl_Position = u_ClipFromScreen * screenPosition;
-        v_Side = a_Side;
+        gl_Position = u_ClipFromScreen * u_ScreenFromLocal * vec4(a_Position / 8.0, 0.0, 1.0);
+        v_TexCoord = a_TexCoord;
       }`;
       
     const fragmentSource = `
       precision mediump float;
 
+      uniform sampler2D u_Texture;
       uniform float u_Opacity;
       
-      varying float v_Side;
+      varying vec2 v_TexCoord;
       
       void main(void) {
-        gl_FragColor = vec4(60.0 / 255.0, 160.0 / 255.0, 220.0 / 255.0, 1.0);
-        gl_FragColor.a *= u_Opacity * (1.0 - length(v_Side));
+        gl_FragColor = texture2D(u_Texture, v_TexCoord);
+        gl_FragColor.a *= u_Opacity;
         gl_FragColor.rgb *= gl_FragColor.a;
+        // gl_FragColor = vec4(v_TexCoord, 0.0, 1.0);
+        // gl_FragColor.a = 0.6;
+        // gl_FragColor.rgb *= gl_FragColor.a;
       }`;
       
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -199,8 +277,6 @@ export class SharedResources extends BaseSharedResources {
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.bindAttribLocation(program, 0, "a_Position");
-    gl.bindAttribLocation(program, 1, "a_Extrude");
-    gl.bindAttribLocation(program, 2, "a_Side");
     gl.linkProgram(program);
     gl.deleteShader(vertexShader);
     gl.deleteShader(fragmentShader);
@@ -214,8 +290,8 @@ export class SharedResources extends BaseSharedResources {
         program,
         uniforms: {
           u_ScreenFromLocal: gl.getUniformLocation(program, "u_ScreenFromLocal")!,
-          u_Rotation: gl.getUniformLocation(program, "u_Rotation")!,
           u_ClipFromScreen: gl.getUniformLocation(program, "u_ClipFromScreen")!,
+          u_Texture: gl.getUniformLocation(program, "u_Texture")!,
           u_Opacity: gl.getUniformLocation(program, "u_Opacity")!
         }
       }
@@ -229,79 +305,56 @@ export class SharedResources extends BaseSharedResources {
 
 export class LocalResources extends BaseLocalResources {
   vertexBuffer: WebGLBuffer | null = null;
-  indexBuffer: WebGLBuffer | null = null;
+  texture: WebGLTexture | null = null;
   u_ScreenFromLocal = mat4.create();
-  u_Rotation = mat4.create();
   u_ClipFromScreen = mat4.create();
-  private flowLines: { position: [number, number]; distance: number; time: number; }[][] | null;
-  indexCount = 0;
+  private imageData: ImageData | null;
 
   constructor(extent: Extent, resolution: number, pixelBlock: any) {
     super(extent, resolution);
 
-    this.flowLines = getFlowLines(pixelBlock);
+    this.imageData = symbolize(pixelBlock);
+
+    // this.imageData = new ImageData(pixelBlock.width, pixelBlock.height);
+    
+    // for (let i = 0; i < pixelBlock.pixels[0].length; i++) {
+    //   this.imageData.data[4 * i + 0] = pixelBlock.pixels[0][i];
+    //   this.imageData.data[4 * i + 1] = pixelBlock.pixels[1][i];
+    //   this.imageData.data[4 * i + 2] = 0;
+    //   this.imageData.data[4 * i + 3] = 255;
+    // }
   }
 
   override attach(gl: WebGLRenderingContext): void {
-    let vertexCount = 0;
-    const vertexData: number[] = [];
-    const indexData: number[] = [];
-
-    defined(this.flowLines);
-
-    for (const line of this.flowLines) {
-      for (let i = 1; i < line.length; i++) {
-        const { position: [x0, y0] } = line[i - 1]!;
-        const { position: [x1, y1] } = line[i]!;
-
-        const l = Math.sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
-        const ex = -(y1 - y0) / l;
-        const ey = (x1 - x0) / l;
-
-        vertexData.push(
-          x0, y0, ex * 32767, ey * 32767, -1,
-          x0, y0, -ex * 32767, -ey * 32767, +1,
-          x1, y1, ex * 32767, ey * 32767, -1,
-          x1, y1, -ex * 32767, -ey * 32767, +1
-        );
-
-        indexData.push(
-          vertexCount + 0,
-          vertexCount + 1,
-          vertexCount + 2,
-          vertexCount + 1,
-          vertexCount + 3,
-          vertexCount + 2
-        );
-
-        vertexCount += 4;
-      }
-    }
-
-    this.indexCount = indexData.length;
-
     const vertexBuffer = gl.createBuffer();
     defined(vertexBuffer);
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Int16Array(vertexData), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Int16Array([
+      0, 0, 0, 32767,
+      this.size[0] * 8, 0, 32767, 32767,
+      0, this.size[1] * 8, 0, 0,
+      this.size[0] * 8, this.size[1] * 8, 32767, 0
+    ]), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-    const indexBuffer = gl.createBuffer();
-    defined(indexBuffer);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexData), gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-    this.flowLines = null;
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    defined(this.imageData);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.imageData);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    this.imageData = null;
+  
     this.vertexBuffer = vertexBuffer;
-    this.indexBuffer = indexBuffer;
-
-    console.log("DONE!");
+    this.texture = texture;
   }
 
   override detach(gl: WebGLRenderingContext): void {
     gl.deleteBuffer(this.vertexBuffer);
-    gl.deleteBuffer(this.indexBuffer);
+    gl.deleteTexture(this.texture);
   }
 }
 
@@ -364,25 +417,16 @@ export class LayerView2D extends BaseLayerView2D<SharedResources, LocalResources
   override renderVisualization(gl: WebGLRenderingContext, renderParams: VisualizationRenderParams, sharedResources: SharedResources, localResources: LocalResources): void {
     defined(localResources.vertexBuffer);
     gl.bindBuffer(gl.ARRAY_BUFFER, localResources.vertexBuffer);
-    gl.vertexAttribPointer(0, 2, gl.SHORT, false, 10, 0);
-    gl.vertexAttribPointer(1, 2, gl.SHORT, true, 10, 4);
-    gl.vertexAttribPointer(2, 1, gl.SHORT, false, 10, 8);
+    gl.vertexAttribPointer(0, 2, gl.SHORT, false, 8, 0);
+    gl.vertexAttribPointer(1, 2, gl.SHORT, true, 8, 4);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     gl.enableVertexAttribArray(0);
     gl.enableVertexAttribArray(1);
-    gl.enableVertexAttribArray(2);
-
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, localResources.indexBuffer);
-
-    gl.disable(gl.CULL_FACE);
 
     mat4.identity(localResources.u_ClipFromScreen);
     mat4.translate(localResources.u_ClipFromScreen, localResources.u_ClipFromScreen, [-1, 1, 0]);
     mat4.scale(localResources.u_ClipFromScreen, localResources.u_ClipFromScreen, [2 / renderParams.size[0], -2 / renderParams.size[1], 1]);
-
-    mat4.identity(localResources.u_Rotation);
-    mat4.rotateZ(localResources.u_Rotation, localResources.u_Rotation, renderParams.rotation);
-
+  
     mat4.identity(localResources.u_ScreenFromLocal);
     mat4.translate(localResources.u_ScreenFromLocal, localResources.u_ScreenFromLocal, [renderParams.translation[0], renderParams.translation[1], 1]);
     mat4.rotateZ(localResources.u_ScreenFromLocal, localResources.u_ScreenFromLocal, renderParams.rotation);
@@ -392,13 +436,16 @@ export class LayerView2D extends BaseLayerView2D<SharedResources, LocalResources
     const solidProgram = sharedResources.programs!["texture"]?.program!;
     gl.useProgram(solidProgram);
     gl.uniformMatrix4fv(sharedResources.programs!["texture"]?.uniforms["u_ScreenFromLocal"]!, false, localResources.u_ScreenFromLocal);
-    gl.uniformMatrix4fv(sharedResources.programs!["texture"]?.uniforms["u_Rotation"]!, false, localResources.u_Rotation);
     gl.uniformMatrix4fv(sharedResources.programs!["texture"]?.uniforms["u_ClipFromScreen"]!, false, localResources.u_ClipFromScreen);
+    gl.uniform1i(sharedResources.programs!["texture"]?.uniforms["u_Texture"]!, 0);
     gl.uniform1f(sharedResources.programs!["texture"]?.uniforms["u_Opacity"]!, renderParams.opacity);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, localResources.texture);
 
     gl.enable(gl.BLEND);
     gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
 
-    gl.drawElements(gl.TRIANGLES, localResources.indexCount, gl.UNSIGNED_SHORT, 0);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
   }
 }
